893f65d2c3ae7793b7a61e5184288970d81b3ca8
Lucas <lucasvalenteds@gmail.com>
2022-07-04 21:12:46 -0300

    Implementing relationship Person-Document

--- src/main/java/com/example/document/Document.java
+++ src/main/java/com/example/document/Document.java
@@ -1,14 +1,18 @@
 package com.example.document;
 
+import com.example.person.Person;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "DOCUMENT")
@@ -24,4 +28,7 @@ public class Document {
 
     @Column(name = "DOCUMENT_CODE")
     private String code;
+
+    @ManyToMany(mappedBy = "documents")
+    private Set<Person> people;
 }
